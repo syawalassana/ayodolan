675469fa72fd0bda345a79940800b2cc7ed500b5
Mokhamad Ariadi <mokhamad27@gmail.com>
2020-08-04 09:18:50 +0700

    fix filter

--- app/Http/Controllers/Api/ObjekWisataController.php
+++ app/Http/Controllers/Api/ObjekWisataController.php
@@ -9,14 +9,28 @@ use App\Http\Resources\ObjekWisataResource;
 
 class ObjekWisataController extends Controller
 {
-   public function index(){
-    $objekwisata = ObjekWisataResource::collection(ObjekWisata::all());
-    return response()->json([
-        'error' => [],
-        'status' => true,
-        'data' => $objekwisata,
-        'massage' => 'data wisata',
-    ]);
-   }
-  
+    public function index(Request $request)
+    {
+        $objek = ObjekWisata::query();
+
+        if ($request->search) {
+            $s = $request->search;
+            $objek->where(function ($q) use ($s) {
+                $q->where('nama_wisata', 'like', '%' . $s . '%')
+                    ->orWhere('deskripsi', 'like', '%' . $s . '%')
+                    ->orWhere('tipe_wisata', $s);
+            });
+        }
+
+        $objek = $objek->get();
+
+        $objekwisata = ObjekWisataResource::collection($objek);
+
+        return response()->json([
+            'error' => [],
+            'status' => true,
+            'data' => $objekwisata,
+            'massage' => 'data wisata',
+        ]);
+    }
 }
